9a69d5b9919550c1649b5a39faee66a9cbba92d0
Tharun Imayavaramban <mitharun16@gmail.com>
2022-11-01 12:28:36 -0700

    Initial Commit

---
+++ Stocks-R-US/Stocks_R_USApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stocks_R_USApp.swift
+//  Stocks-R-US
+//
+//  Created by Tharun Imayavaramban on 11/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Stocks_R_USApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
