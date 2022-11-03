7c308d1777b278a76d34242b793392af01a58666
Tharun Imayavaramban <mitharun16@gmail.com>
2022-11-03 14:45:33 -0700

    Added User View Model

---
+++ Stocks-R-US/ViewModel/UserViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  UserViewModel.swift
+//  Stocks-R-US
+//
+//  Created by Tharun Imayavaramban on 11/3/22.
+//
+
+import Foundation
+class UserViewModel
+{
+    init()
+    {
+        
+    }
+}
